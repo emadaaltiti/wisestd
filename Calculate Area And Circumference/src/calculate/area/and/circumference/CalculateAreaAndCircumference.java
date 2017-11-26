0d1064029cfbe4ac0cb44ae6c962b3bf3fb67db8
emadaltiti <emadtiti@gmail.com>
2017-11-26 08:04:32 +0200

    update function

--- Calculate Area And Circumference/src/calculate/area/and/circumference/CalculateAreaAndCircumference.java
+++ Calculate Area And Circumference/src/calculate/area/and/circumference/CalculateAreaAndCircumference.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Emad Altiti 26/11/2016 7:38 AM 
+ * @author Emad Altiti 26/11/2016 7:38 AM
  */
 public class CalculateAreaAndCircumference {
 
@@ -26,6 +26,23 @@ public class CalculateAreaAndCircumference {
     }
 
     public static void withUserInteraction() {
+//        usingJOptionPane();
+        //Or 
+
+//        usingScanner();
+    }
+
+    //Without user interaction
+    public static void withoutUserInteraction() {
+        double radius = 10.5;
+        double area = Math.PI * (radius * radius);
+        double circumference = Math.PI * 2 * radius;
+        System.out.println("The area of circle is: " + area);
+        System.out.println("The circumference of the circle is:" + circumference);
+
+    }
+
+    public static void usingJOptionPane() {
         double radius = Double.parseDouble(JOptionPane.showInputDialog("Enter the radius: "));
         double area = Math.PI * (radius * radius);
         double circumference = Math.PI * 2 * radius;
@@ -35,8 +52,7 @@ public class CalculateAreaAndCircumference {
 
     }
 
-    //Without user interaction
-    public static void withoutUserInteraction() {
+    public static void usingScanner() {
         System.out.print("Enter the radius: ");
         /*We are storing the entered radius in double
        * because a user can enter radius in decimals
